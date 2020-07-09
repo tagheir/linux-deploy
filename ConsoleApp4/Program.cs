50c9d26120a3584d86f6d0c670f0be7296bbe272
Hisham Asghar <hisham@octacer.com>
2020-07-10 04:54:50 +0500

    Console App Update

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello World! " + DateTime.Now);
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
