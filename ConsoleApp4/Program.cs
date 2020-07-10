c853f3daf4f2579de6aad46f28b7889819c8d068
Hisham Asghar <hisham@octacer.com>
2020-07-10 06:27:46 +0500

    Program Update

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp4
         {
             while (true)
             {
-                Console.WriteLine("Hello World! --- " + DateTime.Now);
+                Console.WriteLine("Hello World! -- Naveen Test 0000 --- " + DateTime.Now);
                 Thread.Sleep(5000);
             }
         }
