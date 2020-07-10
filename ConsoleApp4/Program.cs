49f952b6ff89914982f2c57cc00a8b3421058bf8
Hisham Asghar <hisham@octacer.com>
2020-07-10 05:55:27 +0500

    Update Dotnet Core Deploy

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp4
         {
             while (true)
             {
-                Console.WriteLine("Hello World! --- AGINGGG " + DateTime.Now);
+                Console.WriteLine("Hello World! --- AGINg TEST " + DateTime.Now);
                 Thread.Sleep(5000);
             }
         }
